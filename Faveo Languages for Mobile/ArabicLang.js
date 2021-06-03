325e046f3641bd05c70090dcc18744ed1b2cb69d
Shalini-wordpress <84010979+Shalini-wordpress@users.noreply.github.com>
2021-06-03 17:59:38 +0530

    Create ArabicLang.js

---
+++ Faveo Languages for Mobile/ArabicLang.js
@@ -0,0 +1,121 @@
+export default {
+  /* User login Page */
+  SINGIN: "إشارة",
+  REMEMBERME: "تذكرنى",
+  USERNAME: "اسم المستخدم",
+  PASSWORD: "كلمه السر",
+  LOGIN: "تسجيل الدخول",
+  LDAP: "تسجيل الدخول عبر LDAP",
+  FIELDVALID: "هذه الخانة مطلوبه",
+  SEND: "تسجيل الدخول",
+  USERNAMELABEL: "اسم المستخدم الخاص بك",
+  EMAILUSERNAME: "البريد الإلكتروني / اسم المستخدم",
+  PASSWORDPLACEHOLDER: "كلمتك السرية",
+  LOGINDISCT: "تسجيل الدخول لبدء الجلسة الخاصة بك",
+  CREATEACCOUNT: "تسجيل",
+  CHANGELANGAUGE: "تغيير اللغة",
+  /* Flash Screen page */
+  FLASHTITLE: "تبسيط دعم العملاء",
+  /* 2fa Screen page */
+  TWOFADISCRIPTION:
+    "افتح تطبيق المصادقة ذات العاملين على جهازك لعرض رمز المصادقة الخاص بك والتحقق من هويتك.",
+  TWOFATITLE: "التحقق بخطوتين",
+  TWOFAENTERCODE: "أدخل الرمز إلى Google Authenticator",
+  /* Update Email Screen page */
+  UPDATEEMAIL: "تحديث البريد الإلكتروني",
+  ALREADYACTIVATED: "تم تنشيط الحساب بالفعل",
+  /* Verify Email Screen page */
+  VERIFYCONTAINONE: "مرحبا!",
+  VERIFYCONTAINTWO: "لقد أرسلنا رابط التفعيل إلى",
+  VERIFYCONTAINTHREE:
+    ". يرجى التحقق من صندوق البريد الخاص بك والنقر فوق الارتباط لتنشيط حسابك ثم المحاولة مرة أخرى.",
+  RESENDLINK: "إعادة إرسال رابط التنشيط",
+  VERIFYOR: "أو",
+  VERIFYEMAILUPDATE: "تحديث البريد الإلكتروني",
+  /* OOPS Screen page */
+  OOPS: "الرجاء التواصل مع المسؤول",
+  /* Inbox Screen page */
+  NOTICKETFOUND: "لم يتم العثور على تذاكر.",
+  TICKET: "تذاكر",
+  LOADING: "جار التحميل",
+  /* Drawer Screen page */
+  CREATETICKET: "إنشاء تذكرة",
+  MYTICKET: "تذكرتي",
+  PERSONALINFO: "معلومات شخصية",
+  ABOUTUS: "معلومات عنا",
+  SINGOUT: "خروج",
+  LOGOUT: "تسجيل خروج",
+  LOGOUTMASSAGE: "هل ترغب بالخروج؟",
+  SEARCHTICKET: "تذكرة البحث",
+  RENEWAL: "خطة التجديد",
+  CONFORM: "تؤكد",
+  /* Create Ticket Screen page */
+  CREATETICKET: "إنشاء تذكرة",
+  RETAKE: "إعادة",
+  USEPHOTO: "استعمال",
+  SEE: "انقر فوقي لترى",
+  ATTACHED: "الملفات المرفقة",
+  DONE: "منجز",
+  NOMATCHFOUNDS: "لا يوجد تطابق.",
+  CANCEL: "إلغاء",
+  CLICKHERESEECC: "انقر هنا لرؤية CC المضافة: ",
+  ADDCC: "إضافة CC",
+  DATEFORMATDISCRIPTION: "يجب أن يكون التاريخ بصيغة DD / MM / YYYY ",
+  EMAILFORMATDISCRIPTION: "الرجاء كتابة عنوان بريد إلكتروني صالح",
+  ADDEDCC: "تمت إضافة نسخة",
+  NUMBERDISCRIPTION: "يجب أن يتكون الإدخال من أرقام فقط",
+  EMAILFORMAT: "أدخل عنوان البريد الإلكتروني",
+  ENTERTHEVALUE: "أدخل القيمة",
+  SEARCH: "بحث...",
+  SELECT: "تحديد",
+  SEARCHDATE: "حدد تاريخ",
+  /* Ticket Thread Screen page */
+  DOWNLOAD: "تحميل",
+  TAPREPLY: "الرد",
+  SELECTLANGUAGE: "ضبط",
+  /* User Registation page */
+  REGISTATIONTITLE: "تسجيل",
+  CREATEAUSER : "إنشاء مستخدم",
+  ORGANIZATION: "إنشاء منظمة",
+  SUBMIT: "إرسال",
+  PROFILEEDIT: "تعديل الملف الشخصي",
+  CHOOSELANGUAGE: "اختر اللغة",
+  UPDATEPROFILE: "تحديث الملف",
+  /* Forgot Password page */
+  WEKNOWPASSWORD: "أنا أعرف كلمة المرور الخاصة بي",
+  CHANGEPASSWORD: "إرسال",
+  FORGOTPASSWORD: "هل نسيت كلمة المرور",
+  CPASSWORD: "غير كلمة السر",
+  UPDATE: "تحديث",
+  /* Every Page */
+  APIDISABLED: "API معطل حاليا.",
+  INTERNETERROR: "يرجى التحقق من اتصال الإنترنت.",
+  RETRY: "أعد المحاولة",
+  LANGUAGEPRE: "الرجاء تحديد اللغة المفضلة",
+  /* reply page */
+  LEAVEREPLY: "اترك رد",
+  /* Login Url page */
+  HELPDESK: "عنوان Url الخاص بمكتب المساعدة",
+  PLACEHOLDER: "https://support...",
+  NEED_HELP: "هل تحتاج إلى مساعدة بشأن عنوان URL الخاص بمكتب المساعدة؟",
+  /* HelpLink Component */
+  WELCOME: "أتساءل ماذا سيكون عنوان URL لمكتب المساعدة الخاص بك؟",
+  INSTRUCTIONS:
+    "هذا هو عنوان URL حيث تم تثبيت مكتب المساعدة الخاص بك. يمكنك التواصل مع مسؤول تكنولوجيا المعلومات لمعرفة المزيد.",
+  FOOTER: "لمزيد من المعلومات ، يرجى إرسال بريد إلى support@ladybirdweb.com",
+  EDITTICKET: "تحرير التذكرة",
+  /* Notification Component */
+  NOTIFICATIONTITLE: "تنبيه",
+  NONOTIFICATIONFOUND: "لم يتم العثور على إشعار.",
+  /* About Screen page */
+  FAVEOSUPPORT: "Faveo Support",
+  VERSION: "Version 7.0.0",
+  ABOUTUSDISCRIPTION:
+    "يقع مقر Faveo Helpdesk في بنغالور ، ويوفر للشركات نظام مكتب مساعدة آليًا لإدارة دعم العملاء. تأتي كلمة فافيو من اللاتينية مما يعني أن تكون مواتية. وهو ما يسلط الضوء حقًا على الرؤية والنطاق بالإضافة إلى وظائف المنتج الذي يمثله Faveo. إنه مصمم خصيصًا لتلبية احتياجات الشركات الناشئة والشركات الصغيرة والمتوسطة وتمكينها من خلال أحدث أنظمة الدعم القائمة على التذاكر. في سيناريو بدء التشغيل التنافسي اليوم ، يعد الاحتفاظ بالعملاء أحد التحديات الرئيسية. يعتبر التعامل مع استعلام العميل بجدية هو الفرق بين الاحتفاظ بعلاقة طويلة الأمد أو فقدانها. الشركة مدفوعة بشغف لتوفير أدوات لإدارة استفسارات المستهلكين للحصول على رؤى استراتيجية ومساعدة الشركات على اتخاذ تلك القرارات الحاسمة.",
+  WEBSITE: "موقع الكتروني",
+  WEBDETAIL: "www.faveohelpdesk.com",
+  TITLE: "معلومات عنا",
+  COMPANY_TITLE: "FAVEO HELPDESK",
+  LINK: "www.faveohelpdesk.com",
+  BUTTON: "موقع الكتروني",
+};
