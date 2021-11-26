bdc982cb5bd69ff0f835834511981ea58632a4f5
Shalini Saravanan <84010979+Shalini-wordpress@users.noreply.github.com>
2021-11-26 11:02:54 +0530

    Create FrenchLang.js

---
+++ faveo-web-app/resources/lang/FrenchLang.js
@@ -0,0 +1,122 @@
+export default {
+  /* User login Page */
+  SINGIN: "SE CONNECTER",
+  REMEMBERME: "Souviens-toi de moi",
+  USERNAME: "Nom d'utilisateur",
+  PASSWORD: "Mot de passe",
+  LOGIN: "S'IDENTIFIER",
+  LDAP: "CONNEXION VIA LDAP",
+  FIELDVALID: "Ce champ est requis",
+  SEND: "SE CONNECTER",
+  LOGINDISCT: "Connectez-vous pour démarrer votre session",
+  CREATEACCOUNT: "S'inscrire",
+  CHANGELANGAUGE: "Changer de langue",
+  EMAILUSERNAME: "Email / nom d'utilisateur",
+  /* Flash Screen page */
+  FLASHTITLE: "Simplifier le support client",
+  /* 2fa Screen page */
+  TWOFADISCRIPTION:
+    "Ouvrez l'application d'authentification à deux facteurs sur votre appareil pour afficher votre code d'authentification et vérifier votre identité.",
+  TWOFATITLE: "Vérification en deux étapes",
+  TWOFAENTERCODE: "Entrez le code dans votre authentificateur Google",
+  /* Update Email Screen page */
+  UPDATEEMAIL: "Mettre à jour l'e-mail",
+  ALREADYACTIVATED: "Compte déjà activé",
+  /* Verify Email Screen page */
+  VERIFYCONTAINONE: "salut!",
+  VERIFYCONTAINTWO: "Nous avons envoyé un lien d'activation à",
+  VERIFYCONTAINTHREE:
+    ". Veuillez vérifier votre boîte mail et cliquer sur le lien pour activer votre compte puis réessayer.",
+  RESENDLINK: "RENVOYER LE LIEN D'ACTIVATION",
+  VERIFYOR: "ou",
+  VERIFYEMAILUPDATE: "MISE À JOUR EMAIL",
+  /* OOPS Screen page */
+  OOPS: "Veuillez vous connecter avec l'administrateur",
+  /* Inbox Screen page */
+  NOTICKETFOUND: "Aucun billet trouvé.",
+  TICKET: "Des billets",
+  LOADING: "CHARGEMENT",
+  /* Drawer Screen page */
+  CREATETICKET: "Créer un ticket",
+  MYTICKET: "Mon billet",
+  PERSONALINFO: "Personal Info",
+  ABOUTUS: "À propos de nous",
+  SINGOUT: "Déconnexion",
+  LOGOUT: "Se déconnecter",
+  LOGOUTMASSAGE: "Voulez-vous vous déconnecter?",
+  SEARCHTICKET: "Rechercher un billet",
+  RENEWAL: "Plan de renouvellement",
+  CONFORM: "Confirmer",
+  /* Create Ticket Screen page */
+  CREATETICKET: "Créer un ticket",
+  RETAKE: "Reprendre",
+  USEPHOTO: "Utilise le",
+  SEE: "Cliquez moi pour voir",
+  ATTACHED: "Vos fichiers joints",
+  DONE: "Terminé",
+  NOMATCHFOUNDS: "Pas de résultat trouvé.",
+  CANCEL: "Annuler",
+  CLICKHERESEECC: "Cliquez ici pour voir les CC ajoutés: ",
+  ADDCC: "Ajouter CC",
+  DATEFORMATDISCRIPTION: "La date doit être au format DD/MM/YYYY",
+  EMAILFORMATDISCRIPTION: "Veuillez saisir une adresse e-mail valide",
+  ADDEDCC: "Ajoutée CC",
+  NUMBERDISCRIPTION: "L'entrée ne doit être constituée que de chiffres",
+  EMAILFORMAT: "Entrez l'adresse e-mail",
+  ENTERTHEVALUE: "Entrez la valeur",
+  SEARCH: "Chercher...",
+  SELECT: "Sélectionner",
+  SEARCHDATE: "Sélectionner une date",
+  ORGANIZATION: "organization",
+  /* Ticket Thread Screen page */
+  DOWNLOAD: "Download",
+  TAPREPLY: "Réponse",
+  SELECTLANGUAGE: "Réglage",
+  /* User Registation page */
+  REGISTATIONTITLE: "S'inscrire",
+  CREATEAUSER : "Créer un utilisateur",
+  ORGANIZATION: "Créer une organisation",
+  SUBMIT: "Soumettre",
+  PROFILEEDIT: "Editer le profil",
+  CHOOSELANGUAGE: "Choisissez la langue",
+  UPDATEPROFILE: "Mettre à jour le profil",
+  /* Forgot Password page */
+  WEKNOWPASSWORD: "Je connais mon mot de passe",
+  CHANGEPASSWORD: "Envoyer",
+  FORGOTPASSWORD: "Mot de passe oublié",
+  CPASSWORD: "Changer le mot de passe",
+  UPDATE: "METTRE À JOUR",
+  /* Every Page */
+  APIDISABLED: "L'API est actuellement désactivée.",
+  INTERNETERROR: "Veuillez vérifier la connexion Internet.",
+  RETRY: "Retenter",
+  LANGUAGEPRE: "Veuillez sélectionner la langue préférée",
+  /* reply page */
+  LEAVEREPLY: "Laisser une réponse",
+  /* Login Url page */
+  HELPDESK: "URL du service d'assistance",
+  PLACEHOLDER: "https://support...",
+  NEED_HELP: "Besoin d'aide avec l'URL de votre Helpdesk?",
+  /* HelpLink Component */
+  WELCOME: "Vous vous demandez quelle sera l'URL de votre helpdesk?",
+  INSTRUCTIONS:
+    "Il s'agit de l'URL où votre service d'assistance est installé. Vous pouvez contacter l'administrateur informatique pour en savoir plus.",
+  FOOTER:
+    "Pour plus d'informations, envoyez un e-mail à support@ladybirdweb.com",
+  EDITTICKET: "Modifier le ticket",
+  /* Notification Component */
+  NOTIFICATIONTITLE: "Notification",
+  NONOTIFICATIONFOUND: "Aucune notification trouvée.",
+  /* About Screen page */
+  FAVEOSUPPORT: "Faveo Support",
+  VERSION: "Version 7.0.0",
+  ABOUTUSDISCRIPTION:
+    "Basée à Bangalore, Faveo Helpdesk fournit aux entreprises un système de Helpdesk automatisé pour gérer le support client. Le mot Faveo vient du latin qui signifie être favorable. Ce qui met vraiment en évidence la vision et la portée ainsi que la fonctionnalité du produit que Faveo est. Il est spécialement conçu pour répondre aux besoins des startups et des PME en leur donnant le pouvoir avec un système de support de pointe basé sur des tickets. Dans le scénario de démarrage concurrentiel d’aujourd’hui, la fidélisation de la clientèle est l’un des défis majeurs. Gérer les requêtes des clients avec diligence fait toute la différence entre conserver ou perdre une relation durable. L'entreprise est animée par la passion de fournir des outils de gestion des requêtes des consommateurs pour obtenir des informations stratégiques et d'aider les entreprises à prendre ces décisions décisives.",
+  WEBSITE: "WEBSITE",
+  WEBDETAIL: "www.faveohelpdesk.com",
+  TITLE: "About Us",
+  COMPANY_TITLE: "FAVEO HELPDESK",
+  LINK: "www.faveohelpdesk.com",
+  BUTTON: "WEBSITE",
+  ASSIGN: "Assign",
+};
