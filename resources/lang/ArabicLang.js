92d1bab14949e11ad67cdc9517d5fb590039fe3c
Shalini Saravanan <84010979+Shalini-wordpress@users.noreply.github.com>
2021-11-26 10:53:45 +0530

    Create ArabicLang.js

---
+++ resources/lang/ArabicLang.js
@@ -0,0 +1,121 @@
+export default {
+  /* User login Page */
+  SINGIN: "SIGN IN",
+  REMEMBERME: "Remember me",
+  USERNAME: "Username",
+  PASSWORD: "Password",
+  LOGIN: "LOGIN",
+  LDAP: "LOGIN VIA LDAP",
+  FIELDVALID: "This field is required",
+  SEND: "SIGN IN",
+  LOGINDISCT: "Login to start your session",
+  CREATEACCOUNT: "Register",
+  CHANGELANGAUGE: "Change Language",
+  EMAILUSERNAME: "Email/Username",
+  /* Flash Screen page */
+  FLASHTITLE: "Simplifying Customer Support",
+  /* 2fa Screen page */
+  TWOFADISCRIPTION:
+    "Open the two-factor authentication app on your device to view your authentication code and verify your identity.",
+  TWOFATITLE: "Two Step Verification",
+  TWOFAENTERCODE: "Enter Code To Your Google Authenticator",
+  /* Update Email Screen page */
+  UPDATEEMAIL: "Update Email",
+  ALREADYACTIVATED: "Account already activated",
+  /* Verify Email Screen page */
+  VERIFYCONTAINONE: "Hello!",
+  VERIFYCONTAINTWO: "We have sent an activation link to",
+  VERIFYCONTAINTHREE:
+    ". Please check your mailbox and click on the link to activate your account then try again.",
+  RESENDLINK: "RESEND ACTIVATION LINK",
+  VERIFYOR: "or",
+  VERIFYEMAILUPDATE: "UPDATE EMAIL",
+  /* OOPS Screen page */
+  OOPS: "Please Connect With Admin",
+  /* Inbox Screen page */
+  NOTICKETFOUND: "No tickets found.",
+  TICKET: "Tickets",
+  LOADING: "LOADING",
+  /* Drawer Screen page */
+  CREATETICKET: "Create Ticket",
+  MYTICKET: "My Ticket",
+  PERSONALINFO: "Personal Info",
+  ABOUTUS: "About Us",
+  SINGOUT: "Sign Out",
+  LOGOUT: "Log Out",
+  LOGOUTMASSAGE: "Do you want to logout?",
+  SEARCHTICKET: "Search ticket",
+  RENEWAL: "Renewal Plan",
+  CONFORM: "Confirm",
+  /* Create Ticket Screen page */
+  CREATETICKET: "Create Ticket",
+  RETAKE: "Retake",
+  USEPHOTO: "Use photo",
+  SEE: "Click me to see",
+  ATTACHED: "Your Attached Files",
+  DONE: "Done",
+  NOMATCHFOUNDS: "No match found.",
+  CANCEL: "Cancel",
+  CLICKHERESEECC: "Click here to see added CC: ",
+  ADDCC: "Add CC",
+  DATEFORMATDISCRIPTION: "Date must be in format DD/MM/YYYY",
+  EMAILFORMATDISCRIPTION: "Please type a valid email address",
+  EMAILFORMAT: "Enter the Email Address",
+  ADDEDCC: "Added CC",
+  NUMBERDISCRIPTION: "Input should only consist of numbers",
+  ENTERTHEVALUE: "Enter the value",
+  SEARCH: "Search...",
+  SELECT: "Select",
+  SEARCHDATE: "Select Date",
+  ORGANIZATION: "Create Organization",
+  /* Ticket Thread Screen page */
+  DOWNLOAD: "Download",
+  TAPREPLY: "Reply",
+  SELECTLANGUAGE: "Profile Details",
+  /* User Registation page */
+  REGISTATIONTITLE: "Register",
+  CREATEAUSER : "Create User",
+  SUBMIT: "Submit",
+  PROFILEEDIT: "Edit Profile",
+  CHOOSELANGUAGE: "Choose Language",
+  UPDATEPROFILE: "Update Profile",
+  /* Forgot Password page */
+  WEKNOWPASSWORD: "I know my password",
+  CHANGEPASSWORD: "Send",
+  FORGOTPASSWORD: "Forgot password",
+  CPASSWORD: "Change Password",
+  UPDATE: "UPDATE",
+  /* Every Page */
+  APIDISABLED: "API is currently disabled.",
+  INTERNETERROR: "Please check the internet connection.",
+  RETRY: "Retry",
+  LANGUAGEPRE: "Please Select Preferred Language",
+  /* reply page */
+  LEAVEREPLY: "Leave a reply",
+  /* Login Url page */
+  HELPDESK: "Helpdesk Url",
+  PLACEHOLDER: "https://support...",
+  NEED_HELP: "Need help with your Helpdesk URL?",
+  /* HelpLink Component */
+  WELCOME: "Wondering what will be your helpdesk URL?",
+  INSTRUCTIONS:
+    "This is the URL where your help desk is installed. You can reach out to IT admin to know more.",
+  FOOTER: "For more information, Please drop a mail to support@ladybirdweb.com",
+  EDITTICKET: "Edit Ticket",
+  /* Notification Component */
+  NOTIFICATIONTITLE: "Notifications",
+  NONOTIFICATIONFOUND: "No notifications found.",
+  /* About Screen page */
+  FAVEOSUPPORT: "Faveo Support",
+  VERSION: "Version 7.0.0",
+  ABOUTUSDISCRIPTION:
+    "Headquartered in Bangalore, Faveo Helpdesk provides Businesses with an automated Helpdesk system to manage customer support.  The word Faveo comes from Latin which means to be favourable. Which truly highlights vision and the scope as well as the functionality of the product that Faveo is. It is specifically designed to cater the needs of startups and SME’s empowering them with state of art, ticket based support system. In today’s competitive startup scenario customer retention is one of the major challenges. Handling client query diligently is all the difference between retaining or losing a long lasting relationship. The company is driven with passion of providing tools for managing consumer queries for strategic insights and helping companies take those decisive decisions.",
+  WEBSITE: "WEBSITE",
+  WEBDETAIL: "www.faveohelpdesk.com",
+  TITLE: "About Us",
+  COMPANY_TITLE: "FAVEO HELPDESK",
+  LINK: "www.faveohelpdesk.com",
+  BUTTON: "WEBSITE",
+  CCLIST: "List Of CC",
+  ASSIGN: "Assign",
+};
